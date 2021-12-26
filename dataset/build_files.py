ed4a005c11b60bce37106e22074f1c804af95b5b
Alexandr <knxandr@rambler.ru>
2021-12-26 19:18:57 +0500

    Update build_files.py

--- dataset/build_files.py
+++ dataset/build_files.py
@@ -21,7 +21,7 @@ path = '{}/train'.format(os.getcwd())
 f = open('ded_train.txt', 'w')
 for element in dataset:
   img = cv2.imread('train/{}'.format(element[0]))
-  img = cv2.resize(img, [416, 416], interpolation = cv2.INTER_AREA)
+  img = cv2.resize(img, (416, 416), interpolation = cv2.INTER_AREA)
   cv2.imwrite('train/{}'.format(element[0]), img)
     
   f.write('{}/{} 5,{},5,{},{}\n'.format(path, element[0],
