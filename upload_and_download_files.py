4d8d1c9f2d601aaf8c0b624bfac252eec4730633
Alexandr <knxandr@rambler.ru>
2021-12-26 19:58:15 +0500

    Update upload_and_download_files.py

--- upload_and_download_files.py
+++ upload_and_download_files.py
@@ -43,7 +43,7 @@ def upload_file():
             detect_image(yolo, saved_file, image_path,
                          input_size=YOLO_INPUT_SIZE, show=False,
                          CLASSES=TRAIN_CLASSES, rectangle_colors=(255,0,0))
-            out = filename
+            out = image_path
             return redirect(url_for('uploaded_file',
                                     filename=out))
     return '''
