6684d4ed4e8c20d911e83540788e4b8a543cc18f
Alexandr <knxandr@rambler.ru>
2021-12-26 20:10:37 +0500

    Update utils.py

--- yolov3/utils.py
+++ yolov3/utils.py
@@ -174,6 +174,9 @@ def draw_bbox(image, bboxes, CLASSES=YOLO_COCO_CLASSES, show_label=True, show_co
             # put text above rectangle
             cv2.putText(image, label, (x1, y1-4), cv2.FONT_HERSHEY_COMPLEX_SMALL,
                         fontScale, Text_colors, bbox_thick, lineType=cv2.LINE_AA)
+            
+            cv2.putText(image, label, (10, 10 + fontScale*i), cv2.FONT_HERSHEY_COMPLEX_SMALL,
+                        fontScale, Text_colors, bbox_thick, lineType=cv2.LINE_AA)
 
     return image
 
