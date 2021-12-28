282aa52722c811f8d66c0b7bcfd6f415ae2e53ba
Alexandr <knxandr@rambler.ru>
2021-12-28 16:59:51 +0500

    Update build_files_folders.py

--- dataset/build_files_folders.py
+++ dataset/build_files_folders.py
@@ -25,9 +25,6 @@ os.makedirs(output_path)
 for element in dataset:
   img = cv2.imread('{}/{}'.format(path, element[0]))
   cv2.imwrite('{}/{}/{}'.format(output_path, element[0], element[0]), img)
-    
-  f.write('{}/{} 5,{},5,{},{}\n'.format(path, element[0],
-                                        411, 411,
-                                        element[1]))
+
   
 
