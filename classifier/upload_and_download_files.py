0a71e783b4f4a97452cbd4544c024b240c65e86b
Alexandr <knxandr@rambler.ru>
2021-12-28 19:17:18 +0500

    Update upload_and_download_files.py

--- classifier/upload_and_download_files.py
+++ classifier/upload_and_download_files.py
@@ -2,8 +2,10 @@ import os
 from flask import Flask, request, redirect, url_for
 from werkzeug.utils import secure_filename
 
-import os
 os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+import tensorflow as tf
+assert float(tf.__version__[:3]) >= 2.3
+import tensorflow.lite as tflite
 import os
 import numpy as np
 import cv2
