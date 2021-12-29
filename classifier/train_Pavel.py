86ec088ce119cc409901021d00f0aa58bf60c53d
Alexandr <knxandr@rambler.ru>
2021-12-30 00:23:36 +0500

    Update train_Pavel.py

--- classifier/train_Pavel.py
+++ classifier/train_Pavel.py
@@ -100,14 +100,14 @@ for DROPOUT in DROPOUT_CONFIG:
           train_data = idg.flow_from_dataframe(training_data, directory = dataset_path,
                                                target_size=(IMAGE_SIZE, IMAGE_SIZE),
                                                x_col = "image_name",
-                                               y_col = ['0', '1', '2'], # classes
+                                               y_col = [[0, 1, 2]], # classes
                                                shuffle = True,
                                                class_mode='raw')
           
           test_data = idg.flow_from_dataframe(validation_data, directory = dataset_path,
                                               target_size=(IMAGE_SIZE, IMAGE_SIZE),
                                               x_col = "image_name",
-                                              y_col = ['0', '1', '2'], # classes
+                                              y_col = [[0, 1, 2]], # classes
                                               shuffle = True,
                                               class_mode='raw')
 
