70c2cb541dcaa562cbd4537ff9abd14c14b939d0
Alexandr <knxandr@rambler.ru>
2021-12-29 23:20:47 +0500

    Update train_Pavel.py

--- classifier/train_Pavel.py
+++ classifier/train_Pavel.py
@@ -5,6 +5,7 @@ import tensorflow as tf
 assert float(tf.__version__[:3]) >= 2.3
 import numpy as np
 from sklearn.model_selection import StratifiedKFold as NIKITA
+from sklearn.model_selection import KFold, StratifiedKFold
 
 gpus = tf.config.experimental.list_physical_devices('GPU')
 tf.config.experimental.set_memory_growth(gpus[0], True)
