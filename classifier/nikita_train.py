0486e0d650a4ac56198cd7d54bb7452c3f7fb6a4
Alexandr <knxandr@rambler.ru>
2021-12-29 20:01:49 +0500

    Create nikita_train.py

---
+++ classifier/nikita_train.py
@@ -0,0 +1,212 @@
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
+import tensorflow as tf
+assert float(tf.__version__[:3]) >= 2.3
+import numpy as np
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(gpus[0], True)
+
+
+dataset_path = '/home/alexandr/datasets/santas_2'
+
+###################################
+IMAGE_SIZE = 448
+BATCH_SIZE = 32
+
+VALIDATION_SPLIT = 0.2
+
+FREEZE_EPOCHS = 100
+UNFREEZE_EPOCHS = 500
+
+UNFREEZE_ADAM_LR = 1e-6
+
+OUTPUT_FILE = 'm_8.tflite'
+OUTPUT_FILE_Q = 'm_8_q.tflite'
+
+###################################
+
+
+datagen = tf.keras.preprocessing.image.ImageDataGenerator(
+    rescale=1./255, 
+    validation_split=VALIDATION_SPLIT)
+
+train_generator = datagen.flow_from_directory(
+    dataset_path,
+    target_size=(IMAGE_SIZE, IMAGE_SIZE),
+    batch_size=BATCH_SIZE, 
+    subset='training')
+
+val_generator = datagen.flow_from_directory(
+    dataset_path,
+    target_size=(IMAGE_SIZE, IMAGE_SIZE),
+    batch_size=BATCH_SIZE, 
+    subset='validation')
+
+image_batch, label_batch = next(val_generator)
+image_batch.shape, label_batch.shape
+
+print(train_generator.class_indices)
+
+labels = '\n'.join(sorted(train_generator.class_indices.keys()))
+
+with open('frost_labels.txt', 'w') as f:
+  f.write(labels)
+
+IMG_SHAPE = (IMAGE_SIZE, IMAGE_SIZE, 3)
+
+# Create the base model from the pre-trained MobileNet V2
+base_model = tf.keras.applications.MobileNetV2(input_shape=IMG_SHAPE,
+                                              include_top=False, 
+                                              weights='imagenet')
+
+base_model.trainable = False
+
+model = tf.keras.Sequential([
+  base_model,
+  tf.keras.layers.Conv2D(filters=32, kernel_size=3, activation='relu'),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.GlobalAveragePooling2D(),
+  tf.keras.layers.Dense(units=len(train_generator.class_indices),
+                        activation='softmax')
+])
+
+model.compile(optimizer='adam', 
+              loss='categorical_crossentropy', 
+              metrics=['accuracy'])
+
+model.summary()
+
+print('Number of trainable weights = {}'.format(len(model.trainable_weights)))
+
+history = model.fit(train_generator,
+                    steps_per_epoch=len(train_generator), 
+                    epochs=FREEZE_EPOCHS, # <--------------------------------------
+                    validation_data=val_generator,
+                    validation_steps=len(val_generator))
+
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+print("Number of layers in the base model: ", len(base_model.layers))
+
+base_model.trainable = True
+fine_tune_at = 100
+
+# Freeze all the layers before the `fine_tune_at` layer
+for layer in base_model.layers[:fine_tune_at]:
+  layer.trainable =  False
+
+model.compile(optimizer=tf.keras.optimizers.Adam(UNFREEZE_ADAM_LR),
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.summary()
+
+print('Number of trainable weights = {}'.format(len(model.trainable_weights)))
+
+history_fine = model.fit(train_generator,
+                         steps_per_epoch=len(train_generator), 
+                         epochs=UNFREEZE_EPOCHS, # <--------------------------------------
+                         validation_data=val_generator,
+                         validation_steps=len(val_generator))
+
+acc = history_fine.history['accuracy']
+val_acc = history_fine.history['val_accuracy']
+
+loss = history_fine.history['loss']
+val_loss = history_fine.history['val_loss']
+
+
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
+tflite_model = converter.convert()
+
+with open(OUTPUT_FILE, 'wb') as f:
+  f.write(tflite_model)
+
+# A generator that provides a representative dataset
+def representative_data_gen():
+  dataset_list = tf.data.Dataset.list_files(dataset_path + '/*/*')
+  for i in range(100):
+    image = next(iter(dataset_list))
+    image = tf.io.read_file(image)
+    image = tf.io.decode_jpeg(image, channels=3)
+    image = tf.image.resize(image, [IMAGE_SIZE, IMAGE_SIZE])
+    image = tf.cast(image / 255., tf.float32)
+    image = tf.expand_dims(image, 0)
+    yield [image]
+
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
+# This enables quantization
+converter.optimizations = [tf.lite.Optimize.DEFAULT]
+# This sets the representative dataset for quantization
+converter.representative_dataset = representative_data_gen
+# This ensures that if any ops can't be quantized, the converter throws an error
+converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
+# For full integer quantization, though supported types defaults to int8 only, we explicitly declare it for clarity.
+converter.target_spec.supported_types = [tf.int8]
+# These set the input and output tensors to uint8 (added in r2.3)
+converter.inference_input_type = tf.uint8
+converter.inference_output_type = tf.uint8
+tflite_model = converter.convert()
+
+with open(OUTPUT_FILE_Q, 'wb') as f:
+  f.write(tflite_model)
+
+batch_images, batch_labels = next(val_generator)
+
+logits = model(batch_images)
+prediction = np.argmax(logits, axis=1)
+truth = np.argmax(batch_labels, axis=1)
+
+keras_accuracy = tf.keras.metrics.Accuracy()
+keras_accuracy(prediction, truth)
+
+print("Raw model accuracy: {:.3%}".format(keras_accuracy.result()))
+
+def set_input_tensor(interpreter, input):
+  input_details = interpreter.get_input_details()[0]
+  tensor_index = input_details['index']
+  input_tensor = interpreter.tensor(tensor_index)()[0]
+  # Inputs for the TFLite model must be uint8, so we quantize our input data.
+  # NOTE: This step is necessary only because we're receiving input data from
+  # ImageDataGenerator, which rescaled all image data to float [0,1]. When using
+  # bitmap inputs, they're already uint8 [0,255] so this can be replaced with:
+  #   input_tensor[:, :] = input
+  scale, zero_point = input_details['quantization']
+  input_tensor[:, :] = np.uint8(input / scale + zero_point)
+
+def classify_image(interpreter, input):
+  set_input_tensor(interpreter, input)
+  interpreter.invoke()
+  output_details = interpreter.get_output_details()[0]
+  output = interpreter.get_tensor(output_details['index'])
+  # Outputs from the TFLite model are uint8, so we dequantize the results:
+  scale, zero_point = output_details['quantization']
+  output = scale * (output - zero_point)
+  top_1 = np.argmax(output)
+  return top_1
+
+interpreter = tf.lite.Interpreter(OUTPUT_FILE_Q)
+interpreter.allocate_tensors()
+
+# Collect all inference predictions in a list
+batch_prediction = []
+batch_truth = np.argmax(batch_labels, axis=1)
+
+for i in range(len(batch_images)):
+  prediction = classify_image(interpreter, batch_images[i])
+  batch_prediction.append(prediction)
+
+# Compare all predictions to the ground truth
+tflite_accuracy = tf.keras.metrics.Accuracy()
+tflite_accuracy(batch_prediction, batch_truth)
+print("Quant TF Lite accuracy: {:.3%}".format(tflite_accuracy.result()))
+
+
+
