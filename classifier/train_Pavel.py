4df947dd44526e1325cab75f08fc940144d6b70f
Alexandr <knxandr@rambler.ru>
2021-12-30 01:03:53 +0500

    Update train_Pavel.py

--- classifier/train_Pavel.py
+++ classifier/train_Pavel.py
@@ -49,6 +49,10 @@ f.close()
 
 
 train_data = pd.read_csv('/home/alexandr/datasets/santas/train.csv', sep='\\t', engine='python')
+train_data.loc[0] = np.float32(1., 0., 0.)
+print(train_data)
+train_data.replace(inplace = True)
+
 
 Y = train_data[['class_id']]
 
