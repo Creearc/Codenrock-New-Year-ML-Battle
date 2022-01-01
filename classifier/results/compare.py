644c7486404e4c3f8071d4aef34a7eece0b32a6a
Alexandr <knxandr@rambler.ru>
2022-01-02 01:07:56 +0500

    Update compare.py

--- classifier/results/compare.py
+++ classifier/results/compare.py
@@ -1,23 +1,36 @@
 
-files = ['1.txt',
-         '4.txt']
+def compare(files):
+  res = []
 
-res = []
+  for file in files:
+    res.append(set())
+    f = open(file, 'r')
+    for s in f:
+      res[-1].add(s)
+    f.close()
 
-for file in files:
-  res.append(set())
-  f = open(file, 'r')
-  for s in f:
-    res[-1].add(s)
-  f.close()
+  for i in range(len(res)):
+    for j in range(i+1, len(res)):
+      if i == j:
+        continue
 
-for i in range(len(res)):
-  for j in range(i+1, len(res)):
-    if i == j:
-      continue
+      out = list(res[i].difference(res[j]))
+      for s in out:
+        print(s)
 
-    out = list(res[i].difference(res[j]))
-    print(i, j)
-    for s in out:
-      print(s)
-    print('_____________________________')
+      print('_____________________________')
+  
+  
+
+
+compare(['1.txt',
+         '2.txt'])
+
+compare(['1.txt',
+         '3.txt'])
+
+compare(['1.txt',
+         '4.txt'])
+
+compare(['2.txt',
+         '4.txt'])
