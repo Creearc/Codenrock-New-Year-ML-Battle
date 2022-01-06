b9ee3ad92ad85aededddca33072e97fde86c3a9f
Alexandr <knxandr@rambler.ru>
2022-01-07 00:05:06 +0500

    Update upload_and_download_files.py

--- classifier2/upload_and_download_files.py
+++ classifier2/upload_and_download_files.py
@@ -72,6 +72,7 @@ def detect_image(input_img_path, output_img_path):
     img = cv2.imread(input_img_path, cv2.IMREAD_COLOR)
 
     img_n = cv2.resize(img, (width, height))
+    print(img_n.shape)
 
     y = model.predict(img_n)
     print(y)
