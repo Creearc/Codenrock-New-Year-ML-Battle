dc163704b7c5398139fe3fe8847cd7c3d2a67370
Alexandr <knxandr@rambler.ru>
2022-01-06 20:15:19 +0500

    Update train.py

--- classifier2/train.py
+++ classifier2/train.py
@@ -3,6 +3,7 @@ os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
 import tensorflow as tf
 assert float(tf.__version__[:3]) >= 2.3
+from tensorflow.keras import datasets, layers, models, losses
 import numpy as np
 import pandas as pd
 from sklearn.metrics import f1_score, accuracy_score
