22175b8ebb2ceb13a17a0a4f8887f5591df99ea8
Alexandr <knxandr@rambler.ru>
2022-01-06 22:52:48 +0500

    Update train_efficientnet.py

--- classifier2/train_efficientnet.py
+++ classifier2/train_efficientnet.py
@@ -161,9 +161,11 @@ if not EVAL_ONLY :
 ##      layer.trainable =  False
 
   for UNFREEZE_EPOCHS, LR in UNFREEZE_CONFIG:
-    model.compile(optimizer=tf.keras.optimizers.Adam(LR),
-                  loss='categorical_crossentropy',
-                  metrics=['accuracy'])
+    model.compile(
+    loss="categorical_crossentropy",
+    optimizer=optimizers.RMSprop(lr=LR),
+    metrics=["acc"],
+    )
 
 
     history_fine = model.fit(train_data,
@@ -186,89 +188,5 @@ tf.keras.backend.clear_session()
 
 
 
-converter = tf.lite.TFLiteConverter.from_keras_model(model)
-tflite_model = converter.convert()
-
-with open('results/{}'.format(OUTPUT_FILE), 'wb') as f:
-  f.write(tflite_model)
-
-# A generator that provides a representative dataset
-def representative_data_gen():
-  dataset_list = tf.data.Dataset.list_files(dataset_path + '/*/*')
-  for i in range(100):
-    image = next(iter(dataset_list))
-    image = tf.io.read_file(image)
-    image = tf.io.decode_jpeg(image, channels=3)
-    image = tf.image.resize(image, [IMAGE_SIZE, IMAGE_SIZE])
-    image = tf.cast(image / 255., tf.float32)
-    image = tf.expand_dims(image, 0)
-    yield [image]
-
-converter = tf.lite.TFLiteConverter.from_keras_model(model)
-# This enables quantization
-converter.optimizations = [tf.lite.Optimize.DEFAULT]
-# This sets the representative dataset for quantization
-converter.representative_dataset = representative_data_gen
-# This ensures that if any ops can't be quantized, the converter throws an error
-converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
-# For full integer quantization, though supported types defaults to int8 only, we explicitly declare it for clarity.
-converter.target_spec.supported_types = [tf.int8]
-# These set the input and output tensors to uint8 (added in r2.3)
-converter.inference_input_type = tf.uint8
-converter.inference_output_type = tf.uint8
-tflite_model = converter.convert()
-
-with open('results/{}'.format(OUTPUT_FILE_Q), 'wb') as f:
-  f.write(tflite_model)
-
-batch_images, batch_labels = next(test_data)
-
-logits = model(batch_images)
-prediction = np.argmax(logits, axis=1)
-truth = np.argmax(batch_labels, axis=1)
-
-keras_accuracy = tf.keras.metrics.Accuracy()
-keras_accuracy(prediction, truth)
-
-print("Raw model accuracy: {:.3%}".format(keras_accuracy.result()))
-
-def set_input_tensor(interpreter, input):
-  input_details = interpreter.get_input_details()[0]
-  tensor_index = input_details['index']
-  input_tensor = interpreter.tensor(tensor_index)()[0]
-  # Inputs for the TFLite model must be uint8, so we quantize our input data.
-  # NOTE: This step is necessary only because we're receiving input data from
-  # ImageDataGenerator, which rescaled all image data to float [0,1]. When using
-  # bitmap inputs, they're already uint8 [0,255] so this can be replaced with:
-  #   input_tensor[:, :] = input
-  scale, zero_point = input_details['quantization']
-  input_tensor[:, :] = np.uint8(input / scale + zero_point)
-
-def classify_image(interpreter, input):
-  set_input_tensor(interpreter, input)
-  interpreter.invoke()
-  output_details = interpreter.get_output_details()[0]
-  output = interpreter.get_tensor(output_details['index'])
-  # Outputs from the TFLite model are uint8, so we dequantize the results:
-  scale, zero_point = output_details['quantization']
-  output = scale * (output - zero_point)
-  top_1 = np.argmax(output)
-  return top_1
-
-interpreter = tf.lite.Interpreter('results/{}'.format(OUTPUT_FILE_Q))
-interpreter.allocate_tensors()
-
-# Collect all inference predictions in a list
-batch_prediction = []
-batch_truth = np.argmax(batch_labels, axis=1)
-
-for i in range(len(batch_images)):
-  prediction = classify_image(interpreter, batch_images[i])
-  batch_prediction.append(prediction)
-
-# Compare all predictions to the ground truth
-tflite_accuracy = tf.keras.metrics.Accuracy()
-tflite_accuracy(batch_prediction, batch_truth)
-print("Quant TF Lite accuracy: {:.3%}".format(tflite_accuracy.result()))
 
 
