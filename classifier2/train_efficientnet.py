ce1e630e3d8cc30e88431bf10745f41695b95b26
Alexandr <knxandr@rambler.ru>
2022-01-07 02:23:55 +0500

    Update train_efficientnet.py

--- classifier2/train_efficientnet.py
+++ classifier2/train_efficientnet.py
@@ -24,7 +24,7 @@ inputs = tf.keras.layers.Input(shape=(IMAGE_SIZE, IMAGE_SIZE, 3))
 BATCH_SIZE = 2
 
 FILTERS = 64
-DROPOUT = 0.0
+DROPOUT = 0.2
 
 K_PARTS = 5
 VALIDATION_SPLIT = 0.0
@@ -165,17 +165,30 @@ for k, training_data, validation_data in k_fold_cross_val(data_parts, K_PARTS):
                             validation_steps=len(test_data))
   break 
 
-predictions = model.predict_classes(test_data, verbose=0)
-labels = validation_data['class_id'].to_numpy()
-labels = labels.astype(np.int)
+predictions = []
+labels = []
+
+
+for folder in os.listdir(dataset_path):
+  for file in os.listdir('{}/{}'.format(dataset_path, folder)):
+    
+    img = cv2.imread('{}/{}/{}'.format(dataset_path, folder, file),
+                     cv2.IMREAD_COLOR)
+    
+    img_n = cv2.resize(img, (IMAGE_SIZE, IMAGE_SIZE))
+    img_n = np.expand_dims(img_n, 0)
+
+    y = model.predict_classes(img_n)[0]
+    predictions.append(str(y))
+    labels.append(str(folder))
+
 
 accuracy = accuracy_score(labels, predictions)
 print('Result accuracy: {}'.format(accuracy))
 score = f1_score(labels, predictions, average='weighted')
 print('Result F1: {}'.format(score))
 
-model.save('results/{}__{}.h5'.format(score, OUTPUT_FILE_NAME))
-tf.keras.backend.clear_session()
+model.save('results/{}__{}.pb'.format(score, OUTPUT_FILE_NAME))
 
 
 
