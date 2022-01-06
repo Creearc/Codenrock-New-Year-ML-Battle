8417f2946b6fe8a7d9f5668c1f815c4ade6c0c4a
Alexandr <knxandr@rambler.ru>
2022-01-06 22:56:58 +0500

    Update train_efficientnet.py

--- classifier2/train_efficientnet.py
+++ classifier2/train_efficientnet.py
@@ -21,7 +21,7 @@ IMAGE_SIZE = 448
 IMG_SHAPE = (IMAGE_SIZE, IMAGE_SIZE, 3)
 inputs = tf.keras.layers.Input(shape=(IMAGE_SIZE, IMAGE_SIZE, 3))
 
-BATCH_SIZE = 16# 32
+BATCH_SIZE = 32
 
 FILTERS = 64
 DROPOUT = 0.0
