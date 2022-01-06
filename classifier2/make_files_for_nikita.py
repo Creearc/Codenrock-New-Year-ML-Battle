8b1e48bfb3d647b77c69e0b15a4e8acd582eea27
Alexandr <knxandr@rambler.ru>
2022-01-07 04:50:18 +0500

    Update make_files_for_nikita.py

--- classifier2/make_files_for_nikita.py
+++ classifier2/make_files_for_nikita.py
@@ -24,18 +24,50 @@ class full_net():
     img_n = cv2.resize(img_n, self.img_shape)
     img_n = np.expand_dims(img_n, 0)
 
-    return self.model.predict_classes(img_n)[0]
+    return self.model.predict(img_n)[0]
 
 
 class lite_net():
   def __init__(self, path, img_shape=(448, 448)):
-    interpreter = load_model(path)
-    input_details = interpreter.get_input_details()
+    self.interpreter = load_model(path)
+    input_details = self.interpreter.get_input_details()
     input_shape = input_details[0]['shape']
     self.img_shape = (input_shape[2], input_shape[1])
-    input_index = input_details[0]['index'] 
+    self.input_index = input_details[0]['index'] 
 
   def run(self, img):
     img_n = img.copy()
     img_n = Image.fromarray(cv2.cvtColor(img_n, cv2.COLOR_BGR2RGB))
-    img = img.resize(self.img_shape)
+    img_n = img_n.resize(self.img_shape)
+
+    input_data = np.expand_dims(img_n, axis=0)
+    self.interpreter.set_tensor(self.input_index, input_data)
+    self.interpreter.invoke()
+
+    output_details = self.interpreter.get_output_details()
+    output_data = self.interpreter.get_tensor(output_details[0]['index'])
+    output_data = np.squeeze(output_data)
+
+    return output_data
+
+
+
+dataset_path = '/home/alexandr/datasets/santas_2'
+
+nets = []
+nets.append(lite_net('~/Codenrock-New-Year-ML-Battle/classifier/results/0.9368530020703933__m_4.tflite'))
+
+
+predictions = [[] for i in range(len(nets))]
+labels = []
+
+
+for folder in os.listdir(dataset_path):
+  for file in os.listdir('{}/{}'.format(dataset_path, folder)):
+    
+    img = cv2.imread('{}/{}/{}'.format(dataset_path, folder, file),
+                     cv2.IMREAD_COLOR)
+
+    for net in nets:
+      result = net.run(img)
+      print(result, folder)
