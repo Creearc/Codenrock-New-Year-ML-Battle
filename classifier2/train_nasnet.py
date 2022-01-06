690b91dfd9a1b55e31d355e825a1aaae95d20804
Alexandr <knxandr@rambler.ru>
2022-01-07 02:29:04 +0500

    Update train_nasnet.py

--- classifier2/train_nasnet.py
+++ classifier2/train_nasnet.py
@@ -22,7 +22,7 @@ IMAGE_SIZE = 512
 IMG_SHAPE = (IMAGE_SIZE, IMAGE_SIZE, 3)
 inputs = tf.keras.layers.Input(shape=(IMAGE_SIZE, IMAGE_SIZE, 3))
 
-BATCH_SIZE = 32
+BATCH_SIZE = 24
 
 FILTERS = 64
 DROPOUT = 0.2
