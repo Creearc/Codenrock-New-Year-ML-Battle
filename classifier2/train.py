356086c556aab5a43a82b075f7e6189d58dc15c2
Alexandr <knxandr@rambler.ru>
2022-01-06 20:16:48 +0500

    Update train.py

--- classifier2/train.py
+++ classifier2/train.py
@@ -11,10 +11,18 @@ from sklearn.metrics import f1_score, accuracy_score
 gpus = tf.config.experimental.list_physical_devices('GPU')
 tf.config.experimental.set_memory_growth(gpus[0], True)
 
+###################################
 
+IMAGE_SIZE = 512
+IMG_SHAPE = (IMAGE_SIZE, IMAGE_SIZE, 3)
+
+
+
+
+###################################
 
 model = models.Sequential()
-model.add(layers.Conv2D(6, 5, activation='tanh', input_shape=x_train.shape[1:]))
+model.add(layers.Conv2D(6, 5, activation='tanh', input_shape=IMG_SHAPE))
 model.add(layers.AveragePooling2D(2))
 model.add(layers.Activation('sigmoid'))
 model.add(layers.Conv2D(16, 5, activation='tanh'))
