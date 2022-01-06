c26a6c96bdd3f2eba7ecec7395887d6274a4e5b6
Alexandr <knxandr@rambler.ru>
2022-01-06 20:14:19 +0500

    train new

---
+++ classifier2/train.py
@@ -0,0 +1,26 @@
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
+import tensorflow as tf
+assert float(tf.__version__[:3]) >= 2.3
+import numpy as np
+import pandas as pd
+from sklearn.metrics import f1_score, accuracy_score
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(gpus[0], True)
+
+
+
+model = models.Sequential()
+model.add(layers.Conv2D(6, 5, activation='tanh', input_shape=x_train.shape[1:]))
+model.add(layers.AveragePooling2D(2))
+model.add(layers.Activation('sigmoid'))
+model.add(layers.Conv2D(16, 5, activation='tanh'))
+model.add(layers.AveragePooling2D(2))
+model.add(layers.Activation('sigmoid'))
+model.add(layers.Conv2D(120, 5, activation='tanh'))
+model.add(layers.Flatten())
+model.add(layers.Dense(84, activation='tanh'))
+model.add(layers.Dense(10, activation='softmax'))
+model.summary()
