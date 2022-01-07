1604aa613c9d33a4cbb1804f893d077f304a6426
Alexandr <knxandr@rambler.ru>
2022-01-07 15:48:11 +0500

    Update train_lenet.py

--- classifier2/train_lenet.py
+++ classifier2/train_lenet.py
@@ -71,34 +71,7 @@ def k_fold_cross_val(data_parts, K_PARTS):
                                                                 'class_id' : str(key)},
                                                                ignore_index=True)
 
-    return k, train_data_generator, test_data_generator
-
-    
-k, training_data, validation_data = k_fold_cross_val(data_parts, K_PARTS)
-
-training_data = training_data.sample(frac=1)
-validation_data = validation_data.sample(frac=1)
-
-idg = tf.keras.preprocessing.image.ImageDataGenerator(horizontal_flip=True,
-                                                      rotation_range=15,
-                                                      width_shift_range=0.2,
-                                                      height_shift_range=0.2,
-                                                      zoom_range=[0.8, 1.2],
-                                                      rescale=1./255)
-
-train_data = idg.flow_from_dataframe(training_data,
-                                    target_size=(IMAGE_SIZE, IMAGE_SIZE),
-                                     x_col = "image_name",
-                                     y_col = 'class_id',
-                                     batch_size=BATCH_SIZE, 
-                                     shuffle = False)
-          
-test_data = idg.flow_from_dataframe(validation_data,
-                                    target_size=(IMAGE_SIZE, IMAGE_SIZE),
-                                    x_col = "image_name",
-                                    y_col = 'class_id',
-                                    batch_size=BATCH_SIZE, 
-                                    shuffle = False)
+    yield k, train_data_generator, test_data_generator
 
 model = models.Sequential()
 model.add(layers.Conv2D(6, 5, activation='tanh', input_shape=IMG_SHAPE))
@@ -112,17 +85,41 @@ model.add(layers.Flatten())
 model.add(layers.Dense(84, activation='tanh'))
 model.add(layers.Dense(CLASSES_NUM, activation='softmax'))
 model.summary()
+    
+for k, training_data, validation_data in k_fold_cross_val(data_parts, K_PARTS):
+  training_data = training_data.sample(frac=1)
+  validation_data = validation_data.sample(frac=1)
 
+  idg = tf.keras.preprocessing.image.ImageDataGenerator(horizontal_flip=True,
+                                                        rotation_range=15,
+                                                        width_shift_range=0.2,
+                                                        height_shift_range=0.2,
+                                                        zoom_range=[0.8, 1.2],
+                                                        rescale=1./255)
 
-model.compile(optimizer=tf.keras.optimizers.Adam(LR),
-                  loss='categorical_crossentropy',
-                  metrics=['accuracy'])
+  train_data = idg.flow_from_dataframe(training_data,
+                                      target_size=(IMAGE_SIZE, IMAGE_SIZE),
+                                       x_col = "image_name",
+                                       y_col = 'class_id',
+                                       batch_size=BATCH_SIZE, 
+                                       shuffle = False)
+            
+  test_data = idg.flow_from_dataframe(validation_data,
+                                      target_size=(IMAGE_SIZE, IMAGE_SIZE),
+                                      x_col = "image_name",
+                                      y_col = 'class_id',
+                                      batch_size=BATCH_SIZE, 
+                                      shuffle = False)
 
-history = model.fit(train_data,
-                        steps_per_epoch=len(train_data),
-                        epochs=EPOCHS,
-                        validation_data=test_data,
-                        validation_steps=len(test_data))
+  model.compile(optimizer=tf.keras.optimizers.Adam(LR),
+                    loss='categorical_crossentropy',
+                    metrics=['accuracy'])
+
+  history = model.fit(train_data,
+                          steps_per_epoch=len(train_data),
+                          epochs=EPOCHS,
+                          validation_data=test_data,
+                          validation_steps=len(test_data))
 
 predictions = []
 labels = []
