05b45e5f39d662bb6ad8f70bdc973cf410b73702
Alexandr <knxandr@rambler.ru>
2022-01-07 19:20:22 +0500

    Update train_lenet2.py

--- classifier2/train_lenet2.py
+++ classifier2/train_lenet2.py
@@ -24,9 +24,9 @@ K_PARTS = 5
 
 DROPOUT = 0.2
 
-UNFREEZE_CONFIG = [(3, 1e-2),
-                   (4, 1e-4),
-                   (5, 1e-6)]
+UNFREEZE_CONFIG = [(20, 1e-2),
+                   (18, 1e-4),
+                   (15, 1e-6)]
 
 OUTPUT_FILE_NAME = 'dobrynya_1'
 
