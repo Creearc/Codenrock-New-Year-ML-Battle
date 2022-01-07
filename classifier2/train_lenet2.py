93b5123e007cebca8e1dad5ab3adf14c90498939
Alexandr <knxandr@rambler.ru>
2022-01-08 00:58:42 +0500

    Update train_lenet2.py

--- classifier2/train_lenet2.py
+++ classifier2/train_lenet2.py
@@ -20,13 +20,13 @@ IMG_SHAPE = (IMAGE_SIZE, IMAGE_SIZE, 3)
 
 BATCH_SIZE = 16#32
 
-K_PARTS = 3
+K_PARTS = 5
 
 DROPOUT = 0.2
 
-UNFREEZE_CONFIG = [(1, 1e-2),
-                   (3, 1e-4),
-                   (4, 1e-6)]
+UNFREEZE_CONFIG = [(5, 1e-2),
+                   (6, 1e-4),
+                   (10, 1e-6)]
 
 OUTPUT_FILE_NAME = 'dobrynya_1'
 
