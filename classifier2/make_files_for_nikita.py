a4fa77d95ff120928905d944bb39e49d5a5837ab
Alexandr <knxandr@rambler.ru>
2022-01-07 05:06:47 +0500

    Update make_files_for_nikita.py

--- classifier2/make_files_for_nikita.py
+++ classifier2/make_files_for_nikita.py
@@ -75,7 +75,7 @@ for folder in os.listdir(dataset_path):
     for i in range(len(nets)):
       results = nets[i].run(img)
       predictions[i].append(np.argmax(results))
-      output_files[i].write('{};{}\n'.format(folder, ';'.join([r for r in results])))
+      output_files[i].write('{};{}\n'.format(folder, ';'.join([str(r) for r in results])))
       #print(results, np.argmax(results), folder)
 
 
