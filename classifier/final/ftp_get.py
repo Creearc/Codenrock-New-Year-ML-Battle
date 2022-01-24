38dc89e45482cf6668cc0abe46f07b9d69db32a2
Alexandr <knxandr@rambler.ru>
2022-01-24 17:29:32 +0500

    Update ftp_get.py

--- classifier/final/ftp_get.py
+++ classifier/final/ftp_get.py
@@ -4,15 +4,15 @@ import sys
 PATH = '\\'.join(sys.argv[0].split('\\')[:-1])
 
 ftp = FTP()
-HOST = '192.168.68.207'
+HOST = '192.168.68.206'
 PORT = 21
 
 ftp.connect(HOST, PORT)
 
 print(ftp.login(user='alexandr', passwd='9'))
 
-ftp.cwd('Codenrock-New-Year-ML-Battle/classifier/')
+ftp.cwd('Codenrock-New-Year-ML-Battle/classifier/results')
 
-for i in ['m_6_q.tflite']:
+for i in ['m_7_q.tflite']:
   with open(i, 'wb') as f:
       ftp.retrbinary('RETR ' + i, f.write)
